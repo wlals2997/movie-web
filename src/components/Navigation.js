44c86502a1a22dc9341cbace0520f2b1b29e60dd
wlals2997 <wlals2997@naver.com>
2022-07-27 16:44:59 +0900

    fetch api

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 
-const Navigation = ({ isLoggedIn, userObj }) => {
+const Navigation = ({ isLoggedIn }) => {
  
   return (
     <nav>
--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-const Movie=()=>{
-    return(
-        <span>영화소개</span>
-    )
-}
+const Movie = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://yts.mx/api/v2/list_movies.jsonminimum_rating=8.5&sort_by=year'
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+                <img src={movie.medium_cover_image}/>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                <li>
+                  {movie.genres.map((g) => (
+                    <li key={g}>{g}</li>
+                  ))}
+                </li>
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 export default Movie;
