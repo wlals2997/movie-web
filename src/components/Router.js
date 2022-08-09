cacaeb42e7a3c4a29722916256235362734548df
wlals2997 <wlals2997@naver.com>
2022-08-09 14:37:14 +0900

    라우터, index 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../routes/HomeSection';
 import Auth from '../routes/Auth';
 import Footer from './Footer';
--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import firebase from 'firebase/compat/app';
 import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 reportWebVitals();
